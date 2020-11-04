999cac752b9a088ab78480c96d5e185ec841d695
bdijo <bdijo@D-IMEC-TRAINEE->
2020-11-04 14:43:56 +0200

    Basics of lambda expressions

--- CSharpAdvanced/Program.cs
+++ CSharpAdvanced/Program.cs
@@ -6,7 +6,24 @@ namespace CSharpAdvanced
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // args => expression
+            // number => number * number;
+            // () => ... [no arguments]
+            // x => ... [one argument]
+            // (x, y, z) => ... [multiple arguments]
+            Func<int, int> square = number => number * number;
+            Console.WriteLine(square(5));
+
+            const int factor = 5;
+            Func<int, int> multiplier = n => n * factor;
+            var result = multiplier(10);
+            Console.WriteLine(result);
+        }
+
+        // Method without lambdas
+        static int Square(int number)
+        {
+            return number * number;
         }
     }
 }
