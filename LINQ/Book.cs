6dba6e65c2283e681116c1f2fa2306cb886fb471
bdijo <bdijo@D-IMEC-TRAINEE->
2020-11-05 08:41:52 +0200

    demo of LINQ

---
+++ LINQ/Book.cs
@@ -0,0 +1,21 @@
+﻿namespace LINQ
+{
+    public class Book
+    {
+        private string title;
+        private float price;
+
+        public string Title { get; set; }
+        public float Price { get; set; }
+
+        public Book()
+        {
+        }
+
+        public Book(string title, float price)
+        {
+            Title = title;
+            Price = price;
+        }
+    }
+}
---
+++ LINQ/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var books = new BookRepository().GetBooks();
+
+            // Without LINQ
+            var cheapBooks = new List<Book>();
+            foreach (Book book in books)
+            {
+                if (book.Price < 10)
+                    cheapBooks.Add(book);
+            }
+
+            foreach (Book book in cheapBooks)
+            {
+                Console.WriteLine(book.Title + " " + book.Price);
+            }
+        }
+    }
+}
